96c5779a9a5d6db1d9835ec79aa88e3cdf50ce01
portisk8 <aportillo@desarrollosnea.com>
2019-11-15 17:19:30 -0300

    Fix query consulta 1

--- BDD/Script/Consultas/Consultas/Query1.sql
+++ BDD/Script/Consultas/Consultas/Query1.sql
@@ -11,12 +11,13 @@ where 	tur.paciente = pac.numeroHistoriaClinica and
         pro.especialidad = "Cardiologia" and 
         month(tur.fechaAtencion) = month(curdate()) and 
         year(tur.fechaAtencion) = year(curdate())
-<<<<<<< HEAD
 group by day(tur.fechaAtencion);
 
 
 -- Version MySql 8.0
 
+-- QUERY 1 ****
+-- Obtener el promedio diario de pacientes que hicieron consultas de cardiología en el último mes.
 WITH cantidadConsultasCTE
 AS (
 select  count(*) as cantidad
@@ -28,14 +29,8 @@ where 	tur.paciente = pac.numeroHistoriaClinica and
         pro.especialidad = "Cardiologia" and 
         month(tur.fechaAtencion) = month(curdate()) and 
         year(tur.fechaAtencion) = year(curdate())
-=======
->>>>>>> c4c6bf7140840c364f5c90332f9f7fe859831239
 group by day(tur.fechaAtencion)
 )
 
 select  avg(cantidad)
-<<<<<<< HEAD
 from cantidadConsultasCTE	;
-=======
-from cantidadConsultasCTE	;
->>>>>>> c4c6bf7140840c364f5c90332f9f7fe859831239
